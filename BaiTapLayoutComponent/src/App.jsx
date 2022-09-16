e295a85552f14d4e8e8f357c9fe0255f749c171f
thanhnt <nguyentruongthanh0612@gmail.com>
2022-09-16 20:52:03 +0700

    reactjs_1

---
+++ BaiTapLayoutComponent/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from './BaiTapThucHanhLayout/Body'
+import Footer from './BaiTapThucHanhLayout/Footer'
+import Header from './BaiTapThucHanhLayout/Header'
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
